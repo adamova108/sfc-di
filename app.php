b2bb62ae4e8d2c884267d59325ad80e84dfde057
Adam L <luzsiadam@gmail.com>
2021-09-14 22:07:43 +0200

    + more containerisation

--- app.php
+++ app.php
@@ -7,19 +7,36 @@ require __DIR__ . '/vendor/autoload.php';
 use DiDemo\FriendHarvester;
 use DiDemo\Mailer\SmtpMailer;
 
+/*** START DI CONTAINER ***/
+
 $container = new Pimple();
 
-$container['mailer'] = $container->share(static function () {
+$container['database.dsn'] = 'sqlite:' . __DIR__ . '/data/database.sqlite';
+$container['smtp.server'] = 'smtp.SendMoneyToStrangers.com';
+$container['smtp.user'] = 'smtpuser';
+$container['smtp.password'] = 'smtp';
+$container['smtp.port'] = 465;
+
+$container['mailer'] = $container->share(static function (Pimple $container) {
     return new SmtpMailer(
-        'smtp.SendMoneyToStrangers.com',
-        'smtpuser',
-        'smtppass',
-        '465'
+        $container['smtp.server'],
+        $container['smtp.user'],
+        $container['smtp.password'],
+        $container['smtp.port']
+
     );
 });
 
-$dsn = 'sqlite:' . __DIR__ . '/data/database.sqlite';
-$pdo = new PDO($dsn);
+$container['friend_harvester'] = $container->share(static function (Pimple $container) {
+    return new FriendHarvester($container['pdo'], $container['mailer']);
+});
 
-$friendHarvester = new FriendHarvester($pdo, $container['mailer']);
+$container['pdo'] = $container->share(static function (Pimple $container) {
+    $dsn = 'sqlite:' . __DIR__ . '/data/database.sqlite';
+    return new PDO($container['database.dsn']);
+});
+
+/*** END DI CONTAINER ***/
+
+$friendHarvester = $container['friend_harvester'];
 $friendHarvester->emailFriends();
