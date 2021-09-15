038ee81249d44b9c07c59757b1e2435f36a73af4
Adam L <luzsiadam@gmail.com>
2021-09-15 20:16:33 +0200

    + Separate class version of Pimple

--- app.php
+++ app.php
@@ -15,6 +15,37 @@ require_once __DIR__ . '/app/services.php';
 
 /*** END DI CONTAINER ***/
 
-/** @var FriendHarvester $friendHarvester */
-$friendHarvester = $container['friend_harvester'];
+///** @var FriendHarvester $friendHarvester */
+//$friendHarvester = $container['friend_harvester'];
+//$friendHarvester->emailFriends();
+
+///** @var FriendHarvester $friendHarvester */
+//$friendHarvester = $container['friend_harvester'];
+//$friendHarvester->emailFriends();
+
+/** TODO: Create (1) a separate class AND (2) a SubClass version of the container
+ *        to test IDE autocompletion that ways as well
+ */
+
+/*
+ * Separate class version
+ *
+ class KwdDiContainer {
+
+    private $container;
+
+    public function __construct(Pimple $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFriendHarvester(): FriendHarvester
+    {
+        return $this->container['friend_harvester'];
+    }
+}
+
+$kwdDi = new KwdDiContainer($container);
+$friendHarvester = $kwdDi->getFriendHarvester();
 $friendHarvester->emailFriends();
+*/
